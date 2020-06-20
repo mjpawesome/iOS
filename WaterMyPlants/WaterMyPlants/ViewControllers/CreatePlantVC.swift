c7791ab348e2bff7c72aa5fe5e7f6adc80d45e50
Shawn James <shawn.james@me.com>
2020-06-20 16:07:01 -0400

    Implement imageButton and actionSheet to load an image into view

--- WaterMyPlants/WaterMyPlants/ViewControllers/CreatePlantVC.swift
+++ WaterMyPlants/WaterMyPlants/ViewControllers/CreatePlantVC.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class CreatePlantVC: UIViewController {
-
+    
+    @IBOutlet weak var selectedImage: UIImageView!
+    @IBOutlet weak var addPhotoButton: UIButton!
+    @IBOutlet weak var addAPhotoLabel: UILabel!
+    
+    public var imagePicker: UIImagePickerController? // save reference to it
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
@@ -22,6 +27,51 @@ class CreatePlantVC: UIViewController {
     
     @IBAction func doneButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: false)
+    }
+    
+    @IBAction func addPhotoButtonPressed(_ sender: UIButton) {
+        // hide button
+        addPhotoButton.alpha = 0
+        addAPhotoLabel.alpha = 0
+        presentPhotoLibraryActionSheet()
+    }
+    
+    private func presentPhotoLibraryActionSheet() {
+        // make sure imagePicker is nill
+        if self.imagePicker != nil {
+            self.imagePicker?.delegate = nil
+            self.imagePicker = nil
+        }
+        // init image picker
+        self.imagePicker = UIImagePickerController()
+        // action sheet
+        let actionSheet = UIAlertController(title: "Select Source Type", message: nil, preferredStyle: .actionSheet)
+        // imagePickerActions
+        if UIImagePickerController.isSourceTypeAvailable(.camera) { // need to have real device
+            actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+                self.presentImagePicker(controller: self.imagePicker!, sourceType: .camera)
+            }))
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { _ in
+                self.presentImagePicker(controller: self.imagePicker!, sourceType: .photoLibrary)
+            }))
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            actionSheet.addAction(UIAlertAction(title: "Saved Albums", style: .default, handler: { _ in
+                self.presentImagePicker(controller: self.imagePicker!, sourceType: .savedPhotosAlbum)
+            }))
+        }
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        // present action sheet
+        self.present(actionSheet, animated: true)
+    }
+    
+    // helper for imagePicker
+    func presentImagePicker(controller: UIImagePickerController, sourceType: UIImagePickerController.SourceType) {
+        controller.delegate = self
+        controller.sourceType = sourceType
+        self.present(controller, animated: true)
     }
     
     /*
@@ -34,4 +84,28 @@ class CreatePlantVC: UIViewController {
     }
     */
 
+}
+
+extension CreatePlantVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return self.imagePickerControllerDidCancel(picker)
+        }
+        self.selectedImage.image = image
+        picker.dismiss(animated: true) {
+            // clean up
+            picker.delegate = nil
+            self.imagePicker = nil
+        }
+//        self.uploadImageButton.alpha = 1
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true) {
+            // clean up
+            picker.delegate = nil
+            self.imagePicker = nil
+        }
+    }
+    
 }
