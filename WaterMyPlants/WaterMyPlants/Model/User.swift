f49dd2cf11e34724f50314cb4e77270663b56f77
Mark Poggi <markp@mac.com>
2020-06-20 14:57:08 -0700

    Created initial user model

---
+++ WaterMyPlants/WaterMyPlants/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  WaterMyPlants
+//
+//  Created by Mark Poggi on 6/20/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable, Hashable {
+    var username: String
+    var password: String
+}
+
+struct UserID: Codable {
+    var id: Int?
+}
