1bfc36c44adcc00c408830d77c8f1f84564b7d7c
Joe <squirlyproductions@gmail.com>
2020-06-20 09:11:49 -0400

    launches from launchscreen then to onboard.

--- WaterMyPlants/WaterMyPlants/SceneDelegate.swift
+++ WaterMyPlants/WaterMyPlants/SceneDelegate.swift
@@ -18,6 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+//        let storyboard = UIStoryboard(name: "OnboardAndLogin", bundle: Bundle.main)
+//            guard let authVC =
+//                storyboard.instantiateViewController(withIdentifier: "OnboardVC") as? OnboardViewController else { return }
+//            authVC.modalPresentationStyle = .fullScreen
+//        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -52,5 +57,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
 
-}
 
+}
