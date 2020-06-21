ffac9579f9de44c54ed23d00d06e014ebdcbb220
Shawn James <shawn.james@me.com>
2020-06-21 12:12:45 -0400

    Re-organized and re-added some files

---
+++ WaterMyPlants/WaterMyPlants/Views/Picker/TimeIntervalPicker.swift
@@ -0,0 +1,21 @@
+//
+//  TimeIntervalPicker.swift
+//  WaterMyPlants
+//
+//  Created by Shawn James on 6/21/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import UIKit
+
+class TimeIntervalPicker: UIPickerView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
