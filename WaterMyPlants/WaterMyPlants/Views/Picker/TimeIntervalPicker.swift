e88392a978c5eb2708dff6a39e4194091ba974f4
Shawn James <shawn.james@me.com>
2020-06-21 12:29:54 -0400

    Fix broken file in compile source build settings

--- WaterMyPlants/WaterMyPlants/Views/Picker/TimeIntervalPicker.swift
+++ WaterMyPlants/WaterMyPlants/Views/Picker/TimeIntervalPicker.swift
@@ -9,14 +9,16 @@
 import UIKit
 
 class TimeIntervalPicker: UIPickerView, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    var days: [String] = ["1", "2", "3", "4", "5", "6", "7"]
+    var calendarComponent: [String] = ["Days", "Weeks"]
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int { 2 }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) {
-        if component == 0 {
-            return 7
-        } else {
-            return 2
-        }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+       return 10
     }
-
+    
+    
+    
 }
