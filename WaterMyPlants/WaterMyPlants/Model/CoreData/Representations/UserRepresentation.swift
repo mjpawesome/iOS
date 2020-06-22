7a40db7b52254148cbd997251132c6248a312e91
Mark Poggi <markp@mac.com>
2020-06-22 15:21:32 -0700

    Made changes to UserRepresentation

--- WaterMyPlants/WaterMyPlants/Model/CoreData/Representations/UserRepresentation.swift
+++ WaterMyPlants/WaterMyPlants/Model/CoreData/Representations/UserRepresentation.swift
@@ -10,16 +10,22 @@ import Foundation
 
 struct UserRepresentation: Codable {
     let username: String
-    let password: String
-    let phoneNumber: String
-    let identifier = Int()
+    //optional to avoid storing in CoreData/on server
+    //password will sometimes be transmitted to the server, and sometimes not.
+    let password: String?
+    let phoneNumber: String?
+    let identifier: Int?
+    //token will always be assigned by the login method and only
+    //sent to the server for methods requiring an authenticated user
+    var token: String?
     
     var plants: [PlantRepresentation]?
     
     enum CodingKeys: String, CodingKey {
-         case identifier = "id"
-         case username
-         case password
-         case phoneNumber = "phone_number"
-     }
+        case identifier = "id"
+        case username
+        case password
+        case token
+        case phoneNumber = "phone_number"
+    }
 }
