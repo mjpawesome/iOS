83e4cd8de44a016945371d908c7c590395333ed7
Shawn James <shawn.james@me.com>
2020-06-23 14:40:41 -0400

    Add editButton and implement waterThisPlant button with animation.

--- WaterMyPlants/WaterMyPlants/ViewControllers/DetailVC.swift
+++ WaterMyPlants/WaterMyPlants/ViewControllers/DetailVC.swift
@@ -13,7 +13,8 @@ class DetailVC: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var plantNicknameLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var pushToWaterButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var waterThisPlantButton: UIButton!
     @IBOutlet weak var descriptionLabel: UILabel!
     
     var injectedImage: UIImage?
@@ -39,6 +40,7 @@ class DetailVC: UIViewController {
         plantNicknameLabel.text = plant.nickname // name
         descriptionLabel.text = plant.species ?? "" // species is being treated as a description
         setupTableView(plant: plant)
+        setupWaterThisPlantButton()
     }
     
     private func setupTableView(plant: Plant) {
@@ -50,44 +52,63 @@ class DetailVC: UIViewController {
         self.tableViewCellContent["Next Watering Date"] = nextWateringDate
     }
     
-    private func formatWateringScheduleString(_ days: String) -> String {
-        let daysInt = Int(days)!
-        if daysInt == 1 {
-            return "Every Day"
-        } else if daysInt < 7 {
-            return "Every \(days) Days"
-        } else if daysInt == 7 {
-            return "Every Week"
-        } else {
-            return "Every \(daysInt / 7) Weeks"
-        }
+    private func setupWaterThisPlantButton() {
+        waterThisPlantButton.setTitle("Water", for: .normal)
+        // shadow
+        waterThisPlantButton.layer.shadowColor = UIColor(red: 171/255, green: 178/255, blue: 186/255, alpha: 1).cgColor
+        waterThisPlantButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+        waterThisPlantButton.layer.shadowOpacity = 1
+        waterThisPlantButton.layer.shadowRadius = 4
+        // shape
+        waterThisPlantButton.layer.cornerRadius = waterThisPlantButton.frame.width / 2
+        waterThisPlantButton.layer.masksToBounds = false
     }
     
-    private func calculateNextWateringDate() {
-        
+    private func formatWateringScheduleString(_ days: String) -> String {
+        let daysInt = Int(days)! // convert input to int
+        let day = 1
+        let week = 7
+        // return formatted string
+        if daysInt == day {
+            return "Every Day"
+        } else if daysInt < week {
+            return "Every \(days) Days"
+        } else if daysInt == week {
+            return "Every Week"
+        } else {
+            return "Every \(daysInt / week) Weeks"
+        }
     }
         
     /// called to update any views that may have changes
     private func updateViews() { // FIXME: - should this VC just use observers here instead?
         guard let plant = injectedPlant else { return }
-        togglePushToWaterButton(plant: plant)
+        toggleWaterThisPlantButton(plant: plant)
     }
     
-    private func togglePushToWaterButton(plant: Plant) {
+    private func toggleWaterThisPlantButton(plant: Plant) {
         let parsedFrequencyString = plant.h2oFrequency?.components(separatedBy: ", ") // parse string
         let dateString = parsedFrequencyString?.first // date is first
         let date = dateFormatter.date(from: dateString!)!
-        if date <= Date() {
-            pushToWaterButton.title = "✓ Push to Water"
-            pushToWaterButton.isEnabled = true
+        if date <= Date() { // check to see if the plant date is today or earlier
+            // show
+            waterThisPlantButton.alpha = 1 // FIXME: - remove?
+            waterThisPlantButton.isEnabled = true
+            waterThisPlantButton.isHidden = false
         } else {
-            pushToWaterButton.title = ""
-            pushToWaterButton.isEnabled = false
+            // hide
+            waterThisPlantButton.alpha = 0 // FIXME: - remove?
+            waterThisPlantButton.isEnabled = false
+            waterThisPlantButton.isHidden = true
         }
     }
     
-    @IBAction func pushToWaterButtonPressed(_ sender: UIBarButtonItem) {
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func waterThisPlantButtonPressed(_ sender: UIButton) {
         guard let plant = injectedPlant else { return }
+        waterThisPlantButton.isEnabled = false // we want to stop the user from pressing the button repeatedely
         let parsedFrequencyString = plant.h2oFrequency?.components(separatedBy: ", ") // parse string
         let dateString = parsedFrequencyString?.first // date is first
         let daysString = parsedFrequencyString?.last // days are last
@@ -98,13 +119,28 @@ class DetailVC: UIViewController {
         let nextDateString = dateFormatter.string(from: nextDate!)
         print("New Date : \(nextDateString)")
         let h20Frequency = "\(nextDateString), \(daysString!)" // reformat string to be stored in coreData & network
-        // save
+        // save update
         plant.h2oFrequency = h20Frequency
-        try! CoreDataManager.shared.save()
-        self.updateViews()
-        pushToWaterButton.title = "Done!"
+        try! CoreDataManager.shared.save() // TODO: <-- temporary. proper method call here
+        // animate button to show the user that the UI is responding
+        waterThisPlantButton.setTitle("Done!", for: .normal)
+        performSpringAnimation(forButton_: sender)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.updateViews() // will hide button
+        }
     }
-
+    
+    /// button will appear bounce when called
+    private func performSpringAnimation(forButton_ button: UIButton) {
+        UIButton.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+            button.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }) { (_) in
+            UIButton.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
+                button.transform = CGAffineTransform.identity
+            }, completion: nil)
+        }
+    }
+    
 }
 
 // MARK: - TableView dataSource & delegate methods
