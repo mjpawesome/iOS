f3a91050dca6048c875ffed782fba309c11af6ed
Mark Poggi <markp@mac.com>
2020-06-23 01:14:04 -0700

    WIP updated method to register new user.  Returning a 500 error message.

--- WaterMyPlants/WaterMyPlants/ViewControllers/OnboardingVCs/OnboardViewController.swift
+++ WaterMyPlants/WaterMyPlants/ViewControllers/OnboardingVCs/OnboardViewController.swift
@@ -86,16 +86,16 @@ enum LoginType {
     
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
         let authService = AuthService()
-        guard let password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
-            password.isEmpty == false,
-            let email = emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
-            email.isEmpty == false else { return }
-        
+
         switch selectedLoginType {
-        case .signUp:
-            //FIXME: Not currently getting phone number from signup screen.
+        case .signIn:
+            guard let password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+                password.isEmpty == false,
+                let username = usernameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+                username.isEmpty == false
+                else { return }
             let phoneNumber = "1234567890"
-            authService.registerUser(with: email, and: password, phoneNumber: phoneNumber) {
+            authService.registerUser(username: username, password: password, phoneNumber: phoneNumber) {
                 
                 DispatchQueue.main.async {
                     self.dismiss(animated: true) {
@@ -105,8 +105,13 @@ enum LoginType {
                 }
             }
             
-        case .signIn:
-            authService.loginUser(with: email, password: password) {
+        case .signUp:
+            guard let password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+                password.isEmpty == false,
+                let username = usernameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+                username.isEmpty == false
+                else { return }
+            authService.loginUser(username: username, password: password) {
                 
                 DispatchQueue.main.async {
                     self.dismiss(animated: true) {
